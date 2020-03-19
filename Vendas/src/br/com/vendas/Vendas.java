9014dfade601d6930198edcf9c801454c22ec99a
CarlosAlmeida1987 <trimetria@hotmail.com>
2020-03-19 17:05:28 -0300

    Vendas
    Inicio

---
+++ Vendas/src/br/com/vendas/Vendas.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.vendas;
+
+/**
+ * @author Carlos
+ *
+ */
+public class Vendas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
